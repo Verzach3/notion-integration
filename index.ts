46e8e0f7f009dfedb7735d52bdd26fd8d815c6b0
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2024-02-15 09:34:40 -0500

    update error handling

--- index.ts
+++ index.ts
@@ -1,8 +1,4 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
 import { Client } from "https://cdn.skypack.dev/@notionhq/client";
-import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js";
 import { NotionToMarkdown } from "https://cdn.skypack.dev/notion-to-md";
 import getNotionPageId from "./util/getNotionPageId.ts";
 import { getMdStringFromId } from "./util/getMDStringFromId.ts";
@@ -14,21 +10,22 @@ const n2m = new NotionToMarkdown({
   notionClient: notion,
 });
 Deno.serve(async (req) => {
-  const { link } = await req.json();
+  let parsedReq
+  try {
+    parsedReq = await req.json();
+  } catch (_error) {
+    return new Response(JSON.stringify({error: "Invalid JSON"}),  {
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+  if (!parsedReq.link) {
+    return new Response(JSON.stringify({error: "Invalid JSON"}),  {
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+  const { link } = parsedReq;
   const md = await getMdStringFromId(getNotionPageId(link), n2m);
   return new Response(JSON.stringify({link: link, content: md}),  {
     headers: { "Content-Type": "application/json" },
   });
 });
-
-/* To invoke locally:
-
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
-
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/notion-to-md' \
-    --header 'Authorization: Bearer ' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
-*/
